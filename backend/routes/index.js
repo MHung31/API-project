dc4512e062a93be458544a6e88e82087215d2ac4
Matthew Hung <matthewhung.shop@gmail.com>
2023-08-30 08:51:22 -0400

    starting page hello

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,18 +1,22 @@
 // backend/routes/index.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const apiRouter = require('./api');
+const apiRouter = require("./api");
 
-router.use('/api', apiRouter);
+router.use("/api", apiRouter);
 
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
   res.cookie("XSRF-TOKEN", csrfToken);
-  res.status(200).json({
-    'XSRF-Token': csrfToken
+  return res.status(200).json({
+    "XSRF-Token": csrfToken,
   });
+});
+
+router.get("/", (req, res) => {
+  return res.json("Hello there");
 });
 
 module.exports = router;
