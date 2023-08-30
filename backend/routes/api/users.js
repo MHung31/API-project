aa410a7ed3e47141b19fe70a37d12f18e468f1fa
Matthew Hung <matthewhung.shop@gmail.com>
2023-08-30 06:59:41 -0400

    Add User input validation on user signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,12 +1,32 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+
 const { setTokenCookie, requireAuth } = require("../../utils/auth");
 const { User } = require("../../db/models");
 const router = express.Router();
 
-router.post("/", async (req, res, next) => {
-  const { email, username, password } = req.body;
+const validateSignup = [
+  check("email")
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage("Please provide a valid email."),
+  check("username")
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage("Please provide a username with at least 4 characters."),
+  check("username").not().isEmail().withMessage("Username cannot be an email."),
+  check("password")
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage("Password must be 6 characters or more."),
+  handleValidationErrors,
+];
+
+router.post("", validateSignup, async (req, res) => {
+  const { email, password, username } = req.body;
   const hashedPassword = bcrypt.hashSync(password);
   const user = await User.create({ email, username, hashedPassword });
 
@@ -18,7 +38,9 @@ router.post("/", async (req, res, next) => {
 
   await setTokenCookie(res, safeUser);
 
-  return res.json({ user: safeUser });
+  return res.json({
+    user: safeUser,
+  });
 });
 
 module.exports = router;
