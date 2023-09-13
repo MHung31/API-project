40aa47643a76fdaf82a6d4da5ca9092dab79b7d1
Matthew Hung <matthewhung.shop@gmail.com>
2023-09-13 18:25:22 -0400

    edit review completed

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -10,6 +10,20 @@ const router = express.Router();
 const { setTokenCookie, restoreUser } = require("../../utils/auth");
 const { Spot, Review, Booking, User, Image } = require("../../db/models");
 
+const validateCreateReview = [
+  check("review")
+    .exists({ checkFalsy: true })
+    .withMessage("Review text is required"),
+  check("stars")
+    .exists({ checkFalsy: true })
+    .isInt({
+      min: 1,
+      max: 5,
+    })
+    .withMessage("Stars must be an integer from 1 to 5"),
+  handleValidationErrors,
+];
+
 //Add image to review
 router.post("/:reviewId/images", requireAuth, async (req, res, next) => {
   const { url } = req.body;
@@ -52,5 +66,38 @@ router.post("/:reviewId/images", requireAuth, async (req, res, next) => {
     url: reviewImage.url,
   });
 });
+
+//edit a review
+router.put(
+  "/:reviewId",
+  requireAuth,
+  validateCreateReview,
+  async (req, res, next) => {
+    const { user } = req;
+    const reviewId = Number(req.params.reviewId);
+    const { review, stars } = req.body;
+
+    const currReview = await Review.findByPk(reviewId);
+
+    if (!currReview) {
+      const err = new Error("Review couldn't be found");
+      err.status = 404;
+      return next(err);
+    }
+
+    if (currReview.userId !== user.id) {
+      const err = new Error("Forbidden");
+      err.status = 403;
+      return next(err);
+    }
+
+    await currReview.update({
+      review,
+      stars,
+    });
+
+    return res.json(currReview);
+  }
+);
 
 module.exports = router;
